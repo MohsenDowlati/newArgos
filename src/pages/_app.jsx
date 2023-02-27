e4ec9e8ff31b001772ad6dc733c75aedf0329ab2
mehranfarhdi <mehran.farhadi@aut.ac.ir>
2023-02-27 13:35:42 +0330

    test pip line

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -11,37 +11,11 @@ import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
 
-    const router = useRouter()
-    useEffect(() => {
-        const handleRouteChange = (url) => {
-            gtag.pageview(url)
-        }
-        router.events.on('routeChangeComplete', handleRouteChange)
-        return () => {
-            router.events.off('routeChangeComplete', handleRouteChange)
-        }
-    }, [router.events])
+
     return (
       <Provider store={store}>
           <Head>
-              <script
-                  dangerouslySetInnerHTML={{
-                      __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${gtag.GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-                  }}
-              />
           </Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <Script
-              strategy="afterInteractive"
-              src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-          />
           <Component {...pageProps} />
           <ToastContainer />
       </Provider>
