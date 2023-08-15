ed7b6bb5b3e85ac31d51e8087b9f9e03e8352827
Mohammad <mfarhadi@asu.edu>
2023-08-15 09:39:40 -0700

    test live

--- src/pages/dashboard/cameras.jsx
+++ src/pages/dashboard/cameras.jsx
@@ -8,7 +8,69 @@ import { useEffect, useState } from "react";
 import { GiBleedingEye } from "react-icons/gi";
 import { HiOutlineStatusOnline } from "react-icons/hi";
 import WebSocket from "reconnecting-websocket";
+import React, { useEffect, useState } from 'react';
 
+// Login Page definitions
+const Cameras = () => {
+    const [image, setImage] = useState(null);
+
+    useEffect(() => {
+    async function fetchImage() {
+      try {
+        const response = await fetch("https://api.argos.vision/api/v1/camera/stream/live/");
+        
+        if (!response.ok) {
+          console.error("Error fetching image:", response.statusText);
+          return;
+        }
+
+        const data = await response.json();
+        setImage("data:image/jpeg;base64," + data.image);
+      } catch (error) {
+        console.error("Error occurred:", error);
+      }
+    }
+
+    // Fetch the image immediately
+    fetchImage();
+
+    // Set an interval to fetch a new image every 5 seconds
+    const interval = setInterval(() => {
+      fetchImage();
+    }, 1000); // 5000 milliseconds = 5 seconds
+
+    // Cleanup the interval when the component is unmounted
+    return () => clearInterval(interval);
+  }, []);
+   
+    
+    
+
+    return (
+        <div className="bg-[#212326] w-full">
+            <Navbar Notabsolute={'block'} />
+            <div className="flex h-fit">
+                <div className="      ">
+                <NavigationBar WhichActive={'Camera'} />
+                </div>
+                <div className="bg-[#292c30]  w-full">
+                <div className="h-full">
+                    <div className="App">
+                    {image ? <img src={image} alt="Webcam capture" width="200" height="100" /> : <p>Loading...</p>}
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+
+    );
+}
+
+// Export the Dashboard page
+export default Cameras;
+
+
+/*
 // Login Page definitions
 const Cameras = () => {
     const router = useRouter()
@@ -45,3 +107,4 @@ const Cameras = () => {
 
 // Export the Dashboard page
 export default Cameras;
+*/
